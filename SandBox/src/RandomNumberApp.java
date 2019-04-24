3326c932c34ef1072f5087d7baad63ad0914577d
akidwell <hemmerlea2@gmail.com>
2019-04-24 11:43:30 -0400

    Java stuff

---
+++ SandBox/src/RandomNumberApp.java
@@ -0,0 +1,12 @@
+
+public class RandomNumberApp {
+
+	public static void main(String[] args) {
+		double d = Math.random() * 100;
+				System.out.println("d:");
+				double d100 = d*100;
+				
+
+	}
+
+}
