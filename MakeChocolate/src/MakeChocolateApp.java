42332b98c1b96835a88540d46fabdd150ed4a618
akidwell <hemmerlea2@gmail.com>
2019-05-28 08:46:24 -0400

    updates

--- MakeChocolate/src/MakeChocolateApp.java
+++ MakeChocolate/src/MakeChocolateApp.java
@@ -1,35 +1,21 @@
-import java.util.Scanner;
-
 public class MakeChocolateApp {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to the Make Chocolate App");
-		Scanner sc = new Scanner(System.in);
-		String choice = "y";
-		while (choice.equalsIgnoreCase("y")) {
-			
-		System.out.print("Enter the goal weight of chocolate bars in a box: ");	
-		int goal = sc.nextInt();
-		int big = 5;
-		int small = goal % big;
-		
-		if (small == 0) { System.out.println("-1 (no small bars needed)");
-		} else { System.out.print("small bars = " + small+ ", ");
-		System.out.print("big bars = " + big + ", ");
-		System.out.println("goal = " + goal+ ". ");
-		System.out.println("use " + small + " small chocolate bars");
-		}
-			
-			
-			
-		System.out.print("Contine? y or n: ");	
-		choice = sc.next();	
-			
-		}
-		
+
+			System.out.println(makeChocolate(4,1,7));
+
 		System.out.println("Goodbye");
-		
 
 	}
 
+	public static int makeChocolate(int small, int big, int goal) {
+		if ((small + big * 5) < goal) { 
+			return(-1); 
+		} else if ((big*5) > goal) {
+			return goal % 5;
+		} else {
+		    return goal - big * 5; 
+		}
+	}
 }
